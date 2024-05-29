dbb6c64bee61ae8c249e7b8728d9047e5772021f
grey <charminggw@gmail.com>
2024-05-29 16:05:24 +0900

    update : 2675 문제 리팩토링 w/chatGPT

--- w1/day1_2675.py
+++ w1/day1_2675.py
@@ -1,10 +1,23 @@
+# # 테스트 케이스 갯수 -> t번 반복
+# t = int(input())
+#
+# # t번만큼 입력값 받고 출력 수행
+# for i in range(t):
+#     r, s = input().split()
+#     # 문자열 s를 하나씩 순회하며 r만큼 반복 출력한다.
+#     for a in s:
+#         print(a * int(r), end="")
+#     print()
+
+"""
+ChatGPT 코드 : List Comprehension과 join() 함수를 사용해서
+"""
 # 테스트 케이스 갯수 -> t번 반복
 t = int(input())
 
 # t번만큼 입력값 받고 출력 수행
 for i in range(t):
     r, s = input().split()
-    # 문자열 s를 하나씩 순회하며 r만큼 반복 출력한다.
-    for a in s:
-        print(a * int(r), end="")
-    print()
+    r = int(r)
+    result = ''.join([char * r for char in s])  # 문자열 s의 각 문자를 r번 반복한 문자열을 생성
+    print(result)  # 결과 출력
