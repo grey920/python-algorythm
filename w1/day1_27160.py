e1bdc400bd4ae70fc42f11e7d045672a31391549
grey <charminggw@gmail.com>
2024-05-30 10:57:52 +0900

    add : 27160 문제풀이

---
+++ w1/day1_27160.py
@@ -0,0 +1,29 @@
+"""
+[문제분석]
+- 과일을 키, 과일의 갯수를 값으로 딕셔너리에 담아두고, 값 중에 5가 존재하는 경우 YES, 없으면 NO를 출력
+
+1. 과일을 키로, 값은 모두 0으로 딕셔너리를 만든다
+2. 첫 줄의 숫자만큼 반복해서 입력을 받는다.
+3. 입력받은 문자열을 공백 기준으로 잘라서 첫번째 값인 과일을 찾아 두번째 값을 딕셔너리 값에 누적한다
+4. 모든 입력이 끝난 후 딕셔너리의 값들 중 5가 존재하면 YES, 없으면 NO를 출력한다
+"""
+
+fruit_dict = {"STRAWBERRY": 0, "BANANA": 0, "LIME": 0, "PLUM": 0}
+
+
+def add_fruit_card_in_dict(input_fruit_card):
+    input_fruit, input_fruit_count = input_fruit_card.split()  # 공백 기준으로 과일과 과일 갯수 나누기
+    fruit_dict[input_fruit] += int(input_fruit_count)  # 해당 과일을 딕셔너리 키로 찾아 과일 갯수를 누적한다
+
+
+def has_five_in_dict():
+    return "YES" if 5 in fruit_dict.values() else "NO"
+
+
+# 총 펼쳐진 카드 갯수
+card_count = int(input())
+
+for card in range(card_count):
+    add_fruit_card_in_dict(input())
+
+print(has_five_in_dict())
