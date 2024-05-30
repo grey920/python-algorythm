b8661767c86ce40282c7fc12923b1bb8d9599bc6
grey <charminggw@gmail.com>
2024-05-30 13:44:17 +0900

    refactor : 27160 리팩토링 - 입력 한번에 처리

--- w1/day1_27160.py
+++ w1/day1_27160.py
@@ -7,6 +7,7 @@
 3. 입력받은 문자열을 공백 기준으로 잘라서 첫번째 값인 과일을 찾아 두번째 값을 딕셔너리 값에 누적한다
 4. 모든 입력이 끝난 후 딕셔너리의 값들 중 5가 존재하면 YES, 없으면 NO를 출력한다
 """
+import sys
 
 # 각 과일의 초기 개수 설정
 fruit_counts = {
@@ -25,15 +26,16 @@ def add_fruit_card(fruit_card):
 
 def has_five_fruits():
     """과일 개수가 5개 이상인 과일이 있는지 확인하는 함수"""
-    return "YES" if 5 in fruit_counts.values() else "NO"
+    return 5 in fruit_counts.values()
 
 
-# 입력받은 카드 개수
-num_cards = int(input())
+# input()을 순회하여 받지 않고, 한번에 받아서 처리
+input_cards = sys.stdin.read().strip().split('\n')
+num_cards = int(input_cards[0])
 
 # 각 카드 정보를 입력받아 처리
-for _ in range(num_cards):
-    add_fruit_card(input())
+for card in input_cards[1:num_cards + 1]:
+    add_fruit_card(card)
 
 # 결과 출력
-print(has_five_fruits())
+print("YES" if has_five_fruits() else "NO")
