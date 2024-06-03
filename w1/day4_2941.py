a056187e3bd19e7523a374ab3d7cdb77c821e304
grey <charminggw@gmail.com>
2024-06-03 09:48:19 +0900

    modify : 2941 변수명 리팩토링

--- w1/day4_2941.py
+++ w1/day4_2941.py
@@ -3,10 +3,14 @@ https://www.acmicpc.net/problem/2941
 - 변경 리스트에 해당하는 문자들을 다 상관없는 문자로 (예. a) 변경하고 카운트한다?
 """
 
-input_string = input()
-change_list = ['c=', 'c-', 'dz=', 'd-', 'lj', 'nj', 's=', 'z=']
 
-for el in change_list:
-    input_string = input_string.replace(el, 'A')
+def count_croatian_letters(input_string):
+    change_list = ['c=', 'c-', 'dz=', 'd-', 'lj', 'nj', 's=', 'z=']
 
-print(len(input_string))
+    for char in change_list:
+        input_string = input_string.replace(char, 'A')
+    return len(input_string)
+
+
+input_string = input().strip()
+print(count_croatian_letters(input_string))
